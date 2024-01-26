e789c675bb006d84e8863421a6bd6402e42af0e3
Denis Krestov <rupo@list.ru>
2024-01-26 23:48:32 +0300

    Commit from Webstorm-1

--- src/App.tsx
+++ src/App.tsx
@@ -8,17 +8,17 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
         </p>
+        <p>Lorem ipsum dolor sit amet.</p>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React-2
         </a>
-      </header>
+       </header>
     </div>
   );
 }
