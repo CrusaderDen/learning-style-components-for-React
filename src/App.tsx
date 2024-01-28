dcc68e7f6e65007d2bd33f7e8a4a8e61d4d4a20e
Denis Krestov <rupo@list.ru>
2024-01-28 22:47:37 +0300

    Test style-components API

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,39 @@
 import React from 'react';
-import './App.css';
+import styled from "styled-components";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-        </p>
-        <p>Lorem ipsum dolor sit amet.</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React-2
-        </a>
-       </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Box>
+                <StyledBtn as={'a'} href={'#'}>Link</StyledBtn>;
+                <StyledBtn>Hello</StyledBtn>;
+                <SuperButton>SuperButton</SuperButton>;
+            </Box>
+        </div>
+    );
 }
 
 export default App;
+
+const StyledBtn = styled.button`
+    border: none;
+    background-color: #fb3f78;
+    padding: 10px 20px;
+    color: snow;
+    font-size: 2rem;
+    font-weight: 700;
+`
+const SuperButton = styled(StyledBtn)`
+    border-radius: 5px;
+    background-color: #ffe869;
+    color: #363636;
+`
+
+
+const Box = styled.div`
+    display: flex;
+    height: 100vh;
+    justify-content: center;
+    align-items: center;
+`
